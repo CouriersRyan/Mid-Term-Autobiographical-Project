eb8a37600f866d1cbe0f010ae5367d6371a39e4c
Ryan <zhangryan09@gmail.com>
2021-07-07 14:35:30 -0700

    Restructured Code

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,34 +6,12 @@ using Random = UnityEngine.Random;
 
 public class EnemyController : MonoBehaviour
 {
-    enum Fight
-    {
-        Idle,
-        Jab,
-        Kick
-    }
+    [SerializeField] private float rayLength = 10f;
     
-    [SerializeField] private float moveSpd = 2;
-    [SerializeField] private float jumpForce = 5;
-
-    private bool _isInAction = false;
-    private bool _isGrounded = false;
+    private FighterAction enemyAction;
     private bool _isMove = true;
-    private int _Stunned = 0;
-    
-    private Rigidbody2D _rb;
-    private SpriteRenderer _spriteRenderer;
-    private Animator _anim;
-
-    private int _isWalkingID;
-    private int _jumpID;
-
-    private GameObject _frame = null;
 
     [SerializeField] private GameObject player;
-
-    [SerializeField] private Fight action = Fight.Idle;
-    [SerializeField] private int frameCount = 0;
     [SerializeField] private int randomFrames = 100;
     
     
@@ -41,24 +19,21 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _anim = GetComponent<Animator>();
-
-        _isWalkingID = Animator.StringToHash("isWalking");
-        _jumpID = Animator.StringToHash("Jump");
+        enemyAction = GetComponent<FighterAction>();
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        if (_Stunned <= 0)
-        {
-            _anim.speed = 1;
-            var axisX = Mathf.Sign(player.transform.position.x - transform.position.x);
+        FindPlayer();
+        
+        var axisX = _isMove ? Mathf.Sign(player.transform.position.x - transform.position.x) : 0;
+        
+        var input0 = false;
+        var input1 = false;
 
-            var input0 = false;
-            var input1 = false;
+        if (randomFrames <= 0)
+        {
             if (Random.Range(0, 2) == 1)
             {
                 input0 = true;
@@ -67,157 +42,36 @@ public class EnemyController : MonoBehaviour
             {
                 input1 = true;
             }
-
-            PlayerMove(axisX, false);
-
-            if (randomFrames <= 0)
-            {
-                PlayerFight(input0, input1);
-                randomFrames = Random.Range(50, 80);
-            }
-            else
-            {
-                randomFrames--;
-            }
-
-            if (_isInAction)
-            {
-                if (_frame) _frame.SetActive(false);
-                //TODO: Find and store frames in start so that this is more efficient.
-                var temp = transform.Find(GetStringFromFight(action)).Find("Frame" + frameCount);
-                _frame = temp ? temp.gameObject : null;
-
-                if (_frame)
-                {
-                    _frame.SetActive(true);
-                    frameCount++;
-                    _spriteRenderer.enabled = false;
-                }
-                else
-                {
-                    _isInAction = false;
-                    frameCount = 0;
-                    _spriteRenderer.enabled = true;
-                }
-            }
+            randomFrames = Random.Range(20, 50);
         }
         else
         {
-            _isInAction = false;
-            frameCount = 0;
-            _spriteRenderer.enabled = true;
-            if (_frame) _frame.SetActive(false);
-            _Stunned--;
             randomFrames--;
-            _anim.speed = 0;
-        }
-    }
-
-    private void PlayerMove(float axisX, bool jump)
-    {
-        if (axisX != 0 && !_isInAction && _isMove)
-        {
-            _rb.velocity = new Vector2(axisX * moveSpd, _rb.velocity.y);
-            _anim.SetBool(_isWalkingID, true);
-            transform.localScale = new Vector3(Mathf.Sign(axisX), 1, 1);
-        }
-        else
-        {
-            _anim.SetBool(_isWalkingID, false);
-        }
-
-        if (jump && !_isInAction && _isGrounded)
-        {
-            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            _anim.SetBool(_jumpID, true);
-        }
-    }
-
-    private void PlayerFight(bool input0, bool input1)
-    {
-        if (_isGrounded)
-        {
-            if (input0 && !_isInAction)
-            {
-                _isInAction = true;
-                action = Fight.Jab;
-                _rb.AddForce(-0.1f * Mathf.Sign(transform.localScale.x) * Vector2.right, ForceMode2D.Impulse);
-            }
-
-            if (input1 && !_isInAction)
-            {
-                _isInAction = true;
-                action = Fight.Kick;
-                _rb.AddForce(-0.1f * Mathf.Sign(transform.localScale.x) * Vector2.right, ForceMode2D.Impulse);
-            }
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = true;
-            _anim.SetBool(_jumpID, false);
-        }
-
-        if (other.gameObject.CompareTag("Player"))
-        {
-            _isMove = false;
-        }
-    }
-    
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = false;
-            _anim.SetBool(_jumpID, true);
         }
         
-        if (other.gameObject.CompareTag("Player"))
+        enemyAction.DoActions(axisX, false, input0, input1);
+    }
+    private void FindPlayer()
+    {
+        var dir = Mathf.Sign(transform.localScale.x) * Vector2.right;
+        var origin = new Vector2(transform.position.x, transform.position.y + 1f);
+        LayerMask mask = LayerMask.GetMask("Player");
+
+        RaycastHit2D ray = Physics2D.Raycast(origin, dir, rayLength, mask);
+        
+        Debug.DrawRay(origin, dir * rayLength, Color.red);
+
+        if (ray.collider != null)
+        {
+            Debug.Log("Range Reached!");
+            if (ray.collider.gameObject.CompareTag("Player"))
+            {
+                _isMove = false;
+            }
+        }
+        else
         {
             _isMove = true;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (_Stunned <= 0)
-        {
-            if (other.CompareTag("PlayerJab"))
-            {
-                _Stunned = 15;
-                _rb.AddForce(-Mathf.Sign(other.transform.position.x - transform.position.x) * Vector2.right * 5,
-                    ForceMode2D.Impulse);
-                //Get Stunned
-            }
-
-            if (other.CompareTag("PlayerKick"))
-            {
-                _Stunned = 20;
-                _rb.AddForce(-Mathf.Sign(other.transform.position.x - transform.position.x) * Vector2.right * 7.5f,
-                    ForceMode2D.Impulse);
-                //Get Stunned
-            }
-        }
-    }
-
-
-    private string GetStringFromFight(Fight action)
-    {
-        switch (action)
-        {
-            case Fight.Idle:
-                return "Idle";
-            
-            case Fight.Jab:
-                return "Jab";
-            
-            case Fight.Kick:
-                return "Kick";              
-            default:
-                return null;
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,193 +5,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    enum Fight
-    {
-        Idle,
-        Jab,
-        Kick
-    }
+    //TODO: Make audio as a separate script and use events to have sounds play when player takes an action.
+    // 'SampleEvent?.Invoke()'
     
+    /*TODO: Break up existing code for better architecture. Split up Code so that the inputs are separate from the movement.
+      That way, we can have one movement code, with enemy and player having their own inputs.
+     */
+
+    private FighterAction playerAction;
+
     //Inputs:
     [SerializeField] private KeyCode key0 = KeyCode.J;
     [SerializeField] private KeyCode key1 = KeyCode.K;
     [SerializeField] private KeyCode keyJump = KeyCode.Space;
-
-    [SerializeField] private float moveSpd = 2;
-    [SerializeField] private float jumpForce = 5;
-
-    private bool _isInAction = false;
-    private bool _isGrounded = false;
-    private int _Stunned = 0;
-    
-    private Rigidbody2D _rb;
-    private SpriteRenderer _spriteRenderer;
-    private Animator _anim;
-
-    private int _isWalkingID;
-    private int _jumpID;
-
-    private GameObject _frame = null;
-
-    [SerializeField] private Fight action = Fight.Idle;
-    [SerializeField] private int frameCount = 0;
-    
-    
     
     // Start is called before the first frame update
     private void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _anim = GetComponent<Animator>();
-
-        _isWalkingID = Animator.StringToHash("isWalking");
-        _jumpID = Animator.StringToHash("Jump");
+        playerAction = GetComponent<FighterAction>();
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        if (_Stunned <= 0)
-        {
-            _anim.speed = 1;
+        var axisX = Input.GetAxisRaw("Horizontal");
+        var input0 = Input.GetKey(key0);
+        var input1 = Input.GetKey(key1);
+        var inputJump = Input.GetKey(keyJump);
         
-            var axisX = Input.GetAxisRaw("Horizontal");
-            var input0 = Input.GetKey(key0);
-            var input1 = Input.GetKey(key1);
-            var inputJump = Input.GetKey(keyJump);
-            
-            PlayerMove(axisX, inputJump);
-            
-            PlayerFight(input0, input1);
-
-            if (_isInAction)
-            {
-                if(_frame) _frame.SetActive(false);
-                //TODO: Find and store frames in start so that this is more efficient.
-                var temp = transform.Find(GetStringFromFight(action)).Find("Frame" + frameCount);
-                _frame = temp ? temp.gameObject : null;
-
-                if (_frame)
-                {
-                    _frame.SetActive(true);
-                    frameCount++;
-                    _spriteRenderer.enabled = false;
-                }
-                else
-                {
-                    _isInAction = false;
-                    frameCount = 0;
-                    _spriteRenderer.enabled = true;
-                }
-            }
-        }
-        else
-        {
-            _isInAction = false;
-            frameCount = 0;
-            _spriteRenderer.enabled = true;
-            if (_frame) _frame.SetActive(false);
-            _Stunned--;
-            _anim.speed = 0;
-        }
-    }
-
-    private void PlayerMove(float axisX, bool jump)
-    {
-        if (axisX != 0 && !_isInAction)
-        {
-            _rb.velocity = new Vector2(axisX * moveSpd, _rb.velocity.y);
-            _anim.SetBool(_isWalkingID, true);
-            transform.localScale = new Vector3(Mathf.Sign(axisX), 1, 1);
-        }
-        else
-        {
-            _anim.SetBool(_isWalkingID, false);
-        }
-
-        if (jump && !_isInAction && _isGrounded)
-        {
-            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            _anim.SetBool(_jumpID, true);
-        }
-    }
-
-    private void PlayerFight(bool input0, bool input1)
-    {
-        if (_isGrounded)
-        {
-            if (input0 && !_isInAction)
-            {
-                _isInAction = true;
-                action = Fight.Jab;
-                _rb.AddForce(-0.1f * Mathf.Sign(transform.localScale.x) * Vector2.right, ForceMode2D.Impulse);
-            }
-
-            if (input1 && !_isInAction)
-            {
-                _isInAction = true;
-                action = Fight.Kick;
-                _rb.AddForce(-0.1f * Mathf.Sign(transform.localScale.x) * Vector2.right, ForceMode2D.Impulse);
-            }
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = true;
-            _anim.SetBool(_jumpID, false);
-        }
+        playerAction.DoActions(axisX, inputJump, input0, input1);
     }
     
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = false;
-            _anim.SetBool(_jumpID, true);
-        }
-    }
-    
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (_Stunned <= 0)
-        {
-            if (other.CompareTag("EnemyJab"))
-            {
-                Debug.Log("Hit.");
-                _Stunned = 15;
-                _rb.AddForce(-Mathf.Sign(other.transform.position.x - transform.position.x) * Vector2.right * 5,
-                    ForceMode2D.Impulse);
-                //Get Stunned
-            }
-
-            if (other.CompareTag("EnemyKick"))
-            {
-                _Stunned = 20;
-                _rb.AddForce(-Mathf.Sign(other.transform.position.x - transform.position.x) * Vector2.right * 7.5f,
-                    ForceMode2D.Impulse);
-                //Get Stunned
-            }
-        }
-    }
-
-    private string GetStringFromFight(Fight action)
-    {
-        switch (action)
-        {
-            case Fight.Idle:
-                return "Idle";
-            
-            case Fight.Jab:
-                return "Jab";
-            
-            case Fight.Kick:
-                return "Kick";              
-            default:
-                return null;
-        }
-    }
 }
